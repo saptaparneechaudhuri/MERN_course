1d611d613afbf84030c7ec835634fb541275a857
Saptaparnee Chaudhuri <saptaparnee8@gmail.com>
2021-07-14 18:44:58 +0530

    attempting to add EasyShop

---
+++ EasyShop/screens/Products/SingleProduct.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import { View, StyleSheet, ActivityIndicator, Text } from "react-native";
+
+const ProductContainer = () => {
+  return (
+    <View>
+      <Text>Product Container Screen</Text>
+    </View>
+  );
+};
+
+export default ProductContainer;
